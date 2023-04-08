14b42b6c86bf824c0c931aa8ab3fbdfd2caf025b
Drnagy <128809990+Drnagy@users.noreply.github.com>
2023-04-08 23:34:14 +0200

    Update and rename config.py.example to config.py

--- config.py
+++ config.py
@@ -9,12 +9,12 @@ from typing import List
 # API keys
 
 # Bot token from Bot Father
-TOKEN: str = "123123:YOUR_BOT_TOKEN_HERE"
+TOKEN: str = "6156942689:AAHxJ8YZzKcXJKx-42BV-U1W6GNLqMs0X5U"
 
 # Telegram API ID and API hash
 # Get it from https://my.telegram.org/apps
-API_ID: int = 321321
-API_HASH: str = "YOUR_API_HASH_HERE"
+API_ID: int = 13760933
+API_HASH: str = "17cdd93b30aa096cb9e022fb71bf690c"
 
 # Tenor API key
 # Get it from https://tenor.com/developer/keyregistration
@@ -25,8 +25,8 @@ TENOR_API_KEY: str = ""
 # Admins/sudoers settings
 
 # Sudoers and super sudoers
-SUPER_SUDOERS: List[int] = [123456789]
-SUDOERS: List[int] = [987654321]
+SUPER_SUDOERS: List[int] = [5886877255]
+SUDOERS: List[int] = [5886877255]
 
 # All super sudoers should be sudoers as well
 SUDOERS.extend(SUPER_SUDOERS)
@@ -35,13 +35,13 @@ SUDOERS.extend(SUPER_SUDOERS)
 # Other settings
 
 # Database file path
-DATABASE_PATH = "eduu.db"
+DATABASE_PATH = "mongodb+srv://nagy4462:gotoemail1998@cluster0.9fiktsg.mongodb.net/?retryWrites=true&w=majority"
 
 # Number of updates that can be processed in parallel
 WORKERS = 24
 
 # Chat used for logging
-LOG_CHAT: int = 123456789
+LOG_CHAT: int = -1001788420032
 
 # Prefixes for commands
 # e.g: /command and !command
